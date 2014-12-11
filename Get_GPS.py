e9c867812ee7e3c2614c2f90519b2a3317a1efc0
EngelOfChipolata <buisanguilhem@gmail.com>
2014-12-11 11:00:24 +0100

    Nombre d'essai, VIRGULE, doc
    Mise en place d'une limitation du nombre d'essais (20)
    Ajout de LA virgule qui est censée diminuer la complexité de la recherche (cf. Fabien BONNEVAL)
    Ajout de la doc (en anglais s'il vous plait)

--- Get_GPS.py
+++ Get_GPS.py
@@ -3,22 +3,38 @@ from geopy.geocoders import Nominatim
 
 class GPScoord():
     def __init__(self):
+        '''
+        Set the locator and initialize the number of succes and the number of tries
+        '''
         self.geolocator = Nominatim()
         self.succes = 0
+        self.timestried = 0
 
     def find(self,eqpmt):
+        '''
+        Try 20 times to get the GPS coordinates from the geolocator
+        '''
         try:
-            loc = self.geolocator.geocode(str(eqpmt.adresse)+' Toulouse')
+            self.timestried += 1
+            loc = self.geolocator.geocode(str(eqpmt.adresse)+',Toulouse')
             if loc == None:
-                print('Coord GPS manquantes pour ', eqpmt.name)
+                print('GPS coordinates are missing for', eqpmt.name)
             else:
                 eqpmt.coords = (loc.latitude, loc.longitude)
                 self.succes +=1
+                self.timestried = 0
         except:
-            print('Error : retrying')
-            self.find(eqpmt)
+            if self.timestried <= 20:
+                print('Error : retrying')
+                self.find(eqpmt)
+            else:
+                print('Tried 20 times, can\'t reach out, GPS coorinates are missing for', eqpmt.name)
+                self.timestried = 0
 
     def findall(self, eqpmtlist):
+        '''
+        Read through the list and use 'find' on each equipment in it
+        '''
         for eqpmt in eqpmtlist:
             self.find(eqpmt)
 
