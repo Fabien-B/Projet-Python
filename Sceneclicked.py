a5ef4cdb1ee15b5542e5ef948627ef7801b94403
EngelOfChipolata <buisanguilhem@gmail.com>
2015-01-22 15:17:33 +0100

    Documentation

--- Sceneclicked.py
+++ Sceneclicked.py
@@ -14,16 +14,18 @@ class SceneClickable(QtGui.QGraphicsScene):
         self.selectbackground = None
 
     def clusterclicked(self, cluster):
-        """Si un cluster est cliké envoyer un signal"""
+        """Si un cluster est cliqué envoyer un signal"""
         self.clusterisclicked.emit(cluster)
 
     def equipclicked(self, equipoint):
+        """Si un équipement est cliqué envoyer un signal et dessiner l'icone de selection"""
         self.equipointisclicked.emit(equipoint)
         if equipoint.equipment:
             self.giveEqCoordsSignal.emit(equipoint.equipment.coords)
         self.draw_back_equip_select(equipoint)
 
     def draw_back_equip_select(self, equipoint):
+        """Dessine l'icone de selection sur le point 'equipoint'"""
         equipoint.selected = True
         if self.selectbackground:
             self.removeItem(self.selectbackground)
@@ -31,4 +33,5 @@ class SceneClickable(QtGui.QGraphicsScene):
         self.addItem(self.selectbackground)
 
     def bgclicked(self, background):
+        """Si le background d'un cluster explosé est cliqué, envoie un signal"""
         self.backgroundclicked.emit(background)
